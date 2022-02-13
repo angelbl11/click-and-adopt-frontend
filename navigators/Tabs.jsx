b6fc3ff1f7372e5913c6bf7bebee4da8a9143ab4
AngelBL11 <angelbl1611@gmail.com>
2022-02-12 20:30:51 -0600

    fixed nav-validation login/signup forms

---
+++ navigators/Tabs.jsx
@@ -0,0 +1,127 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AdopterProfile from "../screens/AdopterProfile";
+import CardsScreen from "../screens/CardsScreen";
+import ChatScreen from "../screens/ChatScreen";
+import LikesScreen from "../screens/LikesScreen";
+const Tab = createBottomTabNavigator();
+
+import { NativeBaseProvider, View, Image } from "native-base";
+import { Colors } from "./../components/Styles";
+const { primary, tertiary, brand, darkLight } = Colors;
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 8,
+          elevation: 0,
+          backgroundColor: primary,
+        },
+        headerStyle: {
+          backgroundColor: "transparent",
+        },
+        headerTintColor: tertiary,
+        headerTransparent: "true",
+        headerTitle: "",
+        headerLeftContainerStyle: {
+          paddingLeft: 10,
+        },
+      }}
+      initialRouteName="Profile"
+    >
+      <Tab.Screen
+        name="Inicio"
+        component={CardsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NativeBaseProvider>
+              <View>
+                <Image
+                  source={require("./../assets/home.png")}
+                  resizeMode="contain"
+                  alt="home-logo"
+                  style={{
+                    width: 45,
+                    height: 55,
+                    tintColor: focused ? brand : darkLight,
+                  }}
+                ></Image>
+              </View>
+            </NativeBaseProvider>
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Likes"
+        component={LikesScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NativeBaseProvider>
+              <View>
+                <Image
+                  source={require("./../assets/like.png")}
+                  resizeMode="contain"
+                  alt="like-logo"
+                  style={{
+                    width: 35,
+                    height: 55,
+                    tintColor: focused ? brand : darkLight,
+                  }}
+                ></Image>
+              </View>
+            </NativeBaseProvider>
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Chats"
+        component={ChatScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NativeBaseProvider>
+              <View>
+                <Image
+                  source={require("./../assets/chat.png")}
+                  resizeMode="contain"
+                  alt="chat-logo"
+                  style={{
+                    width: 35,
+                    height: 55,
+                    tintColor: focused ? brand : darkLight,
+                  }}
+                ></Image>
+              </View>
+            </NativeBaseProvider>
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Perfil"
+        component={AdopterProfile}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <NativeBaseProvider>
+              <View>
+                <Image
+                  source={require("./../assets/profile.png")}
+                  resizeMode="contain"
+                  alt="profile-logo"
+                  style={{
+                    width: 35,
+                    height: 55,
+                    tintColor: focused ? brand : darkLight,
+                  }}
+                ></Image>
+              </View>
+            </NativeBaseProvider>
+          ),
+        }}
+      ></Tab.Screen>
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
